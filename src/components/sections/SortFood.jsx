83d510fa20f1f8b4f1668fb7aa1d7c300448c50a
Kamrul Hasan <kamrul3496hasan@gmail.com>
2023-11-06 23:25:28 +0600

    sort food section added

---
+++ src/components/sections/SortFood.jsx
@@ -0,0 +1,26 @@
+import PropTypes from "prop-types";
+
+function SortFood({ handleSorting }) {
+  return (
+    <div>
+      <label className="flex flex-col md:items-end">
+        <span className="font-semibold ">Sort Food on Expiry:</span>
+        <select
+          className="select select-bordered join-item focus:outline-none bg-primary-color hover:bg-white text-white hover:text-black "
+          onChange={handleSorting}
+          defaultValue={"Sort product on expiry"}
+        >
+          <option disabled> --Sort-- </option>
+          <option>Sort Expiry</option>
+          <option>Long Expiry</option>
+        </select>
+      </label>
+    </div>
+  );
+}
+
+SortFood.propTypes = {
+  handleSorting: PropTypes.func,
+};
+
+export default SortFood;
